7bd5a0fea1595ed9da9f1302570a9eff7416c674
Michał Szwejk <mszwejk1@gmail.com>
2025-12-11 16:37:37 +0100

    add basic precision-recall calculations

---
+++ src/anaylsis/pr.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+import numpy as np
+from sklearn.metrics import auc, precision_recall_curve
+
+from src.model.classifier import Classifier
+
+
+@dataclass
+class PRMetrics:
+    precision: np.ndarray
+    recall: np.ndarray
+    auc_score: float
+
+
+def calculate_pr_metrics(
+    X_test: np.ndarray, y_test: np.ndarray, classifier: Classifier
+) -> PRMetrics:
+    proba = classifier.predict_proba(X_test)[:, 1]
+    precision, recall, _ = precision_recall_curve(y_test, proba)
+    auc_score = auc(recall, precision)
+
+    return PRMetrics(precision, recall, float(auc_score))
